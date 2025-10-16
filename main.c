aceed78cce2b975b44ecf385f060f46a1512bb1c
kyunyoung000 <kyunyoung@sookmyung.ac.kr>
2025-10-16 15:37:03 +0900

    07-3

--- main.c
+++ main.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{ int i;
+int inc(int counter);
 
-  for(i=0; i<5; i++)
-  {
-           static int temp = 1;
-           printf("temp = %d\n", temp);
-           temp++;
-  }
+int main(int argc, char *argv[])
+{ 
+  int i = 10;
   
-  
+  printf("함수 호출 전 i = %d\n", i);
+  i = inc(i);
+  printf("함수 호출 후 i = %d\n", i);
+   
+
   system("PAUSE");	
   return 0;
+}
+
+int inc(int counter)
+{
+    counter++;
+    return counter;
 }
